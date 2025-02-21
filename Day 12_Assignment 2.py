19909b6dde5ba7527bff40087e5bfcadb5c12d17
YamunaGit2002 <yamunamane13@gmail.com>
2025-02-21 12:31:48 +0530

    Add files via upload

---
+++ Day 12_Assignment 2.py
@@ -0,0 +1,10 @@
+#Write a Python program to convert a list to a tuple. 
+#Defining the list
+listx = [5, 10, 7, 4, 15, 3]
+#Converting list to tuple
+tuplex = tuple(listx)
+#Print the result
+print("Output:", tuplex)
+
+#Output
+Output: (5, 10, 7, 4, 15, 3)
